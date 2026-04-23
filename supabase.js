9a1a8d1ddff702541a563d103378f0d0a0f3c196
brunofeitosa123-a11y <brunofeitosa123@gmail.com>
2026-04-23 17:01:15 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
-h1,
-p {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const SUPABASE_URL = 'https://oldgaobxydouwqafwrdn.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_MT9QHkf_GnqUixbUYb1B7A_Pa77_ga1'
+
+export cont supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
