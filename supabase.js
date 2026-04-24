983dea1453d5632a232966824bab7e01e4c35f35
brunofeitosa123-a11y <brunofeitosa123@gmail.com>
2026-04-24 01:37:00 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://ncxmsvnshjsxaclffssj.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jeG1zdm5zaGpzeGFjbGZmc3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMTg1MTIsImV4cCI6MjA4OTY5NDUxMn0.Gr9Uj5mtwshipsE-ya4xiB9AVabCtKPs_K_WS6fsmmc'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jeG1zdm5zaGpzeGFjbGZmc3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0MTcwNTMsImV4cCI6MjA1Nzk5MzA1M30.eyJpc3MiOiJzdXBhYmFzZSJ9'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
